89aa3e1d4867c9e5ba933ee787422d57a2525554
Lucas Bercê de Jesus <lucasberce71@gmail.com>
2023-04-09 14:32:08 -0300

    feat: Create a formatter util function

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,6 +3,8 @@ import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 
+import { priceFormatter } from "../../utils/formatter";
+
 import * as S from "./styles";
 
 export function Summary() {
@@ -35,7 +37,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
-                <strong>{summary.income}</strong>
+                <strong>{priceFormatter.format(summary.income)}</strong>
             </S.SummaryCard>
 
             <S.SummaryCard>
@@ -44,7 +46,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
-                <strong>{summary.outcome}</strong>
+                <strong>{priceFormatter.format(summary.outcome)}</strong>
             </S.SummaryCard>
 
             <S.SummaryCard variant="green">
@@ -53,7 +55,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
-                <strong>{summary.total}</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </S.SummaryCard>
         </S.SummaryContainer>
     );
